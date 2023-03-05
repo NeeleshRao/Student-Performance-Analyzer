fa35970103939a6ac8bcb10edcba27b93ff4be65
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2023-03-05 11:08:06 +0530

    Initial Commit

---
+++ Teacher/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, current_app
+from flask_sqlalchemy import SQLAlchemy
+import os, pytest
+from flask_login import LoginManager
+import pymysql
+
+secret_key = os.urandom(32)
+
+app = Flask(__name__)
+        
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bablu2002@localhost/school'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secret_key
+app.config['UPLOAD_FOLDER'] = "C:/Users/Neelesh Thonse Rao/Desktop/programming/Student Performance Analyzer/after cie/nextone/Teacher/static/uploads/"
+app.config['SAMPLES_FOLDER'] = "C:/Users/Neelesh Thonse Rao/Desktop/programming/Student Performance Analyzer/after cie/nextone/Teacher/static/samples/"
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from Teacher import routes
