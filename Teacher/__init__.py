6e0e1e0bc9541f433003452144ca519bc21220ef
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-03-28 11:58:49 +0530

    Removed password

--- Teacher/__init__.py
+++ Teacher/__init__.py
@@ -8,7 +8,7 @@ secret_key = os.urandom(32)
 
 app = Flask(__name__)
         
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bablu2002@localhost/school'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/school'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key
 app.config['UPLOAD_FOLDER'] = "C:/Users/Neelesh Thonse Rao/Desktop/programming/Student Performance Analyzer/after cie/nextone/Teacher/static/uploads/"
